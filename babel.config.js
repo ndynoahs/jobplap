b3f510ff930e3714c44c105a35618998770b2426
kingsley <ndynoahs@gmail.com>
2023-12-31 23:38:19 +0100

    fixed env errors

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["expo-router/babel"],
+    plugins: [
+    ["module:react-native-dotenv", {
+      "envName": "APP_ENV",
+      "moduleName": "@env",
+      "path": ".env",
+      "safe": false,
+      "allowUndefined": true,
+      "verbose": false
+    }]
+  ]
+    
   };
 };
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,14 +1,13 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-// import { RAPID_API_KEY } from '@env'
-// const rapidApiKey = RAPID_API_KEY;
+import { RAPID_API_KEY } from '@env'
+const rapidApiKey = RAPID_API_KEY;
 
 const useFetch = (endpoint, query) => {
     const [ data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
-    // const rapidApiKey = process.env.RAPID_API_KEY
-    
+
     const options = {
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
@@ -16,7 +15,7 @@ const useFetch = (endpoint, query) => {
             ...query
         },
         headers: {
-          'X-RapidAPI-Key': '843545256dmshccc680579505eb6p14ab67jsnb09a063ff767',
+          'X-RapidAPI-Key': [RAPID_API_KEY],
           'X-Rap idAPI-Host': 'jsearch.p.rapidapi.com'
         }
       };
